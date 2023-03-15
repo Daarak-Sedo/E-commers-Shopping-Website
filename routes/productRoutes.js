2edb5dde724d8c5ad2d92a40f23da170cfe4d78a
Bhuwan Singh Samant <bhuwansinghsamant@Bhuwans-MacBook-Air.local>
2023-03-15 17:56:55 +0530

    ecomersDay1

---
+++ routes/productRoutes.js
@@ -0,0 +1,90 @@
+import express from "express";
+import {
+  brainTreePaymentController,
+  braintreeTokenController,
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  productPhotoController,
+  realtedProductController,
+  searchProductController,
+  updateProductController,
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidableExpressMiddleWare from "express-formidable";
+// import multer from "multer";
+
+// const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+
+//   },
+//   filename: function(req, file, cb) {
+
+//   }
+// });
+
+// const upload = multer({storage: storage})
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidableExpressMiddleWare(),
+  // upload.any(),    // to recive all Type of adata form - form data ---  without Multer , it will not recive any body Data
+  createProductController
+);
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidableExpressMiddleWare(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+//payments routes
+//token
+router.get("/braintree/token", braintreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+
+export default router;
